a362aaf6f46df21a3cabc7668c446c0bc4c8941b
My Name <cpgsom819@gmail.com>
2023-05-18 18:28:08 +0800

    fetching data

---
+++ script.js
@@ -0,0 +1,14 @@
+// hEB9wQM6IFynOtJSgVYx7PiqFISKgiCW6xNLZ314P65BfTLoQgYEUlC4
+
+const apiKey=hEB9wQM6IFynOtJSgVYx7PiqFISKgiCW6xNLZ314P65BfTLoQgYEUlC4
+
+const perpage =15
+let currentPage = 1
+
+
+const getImages = (apiURL)
+
+
+
+
+getImages(`https://api.pexels.com/v1/curated?page=${currentPage}per_page=${perpage}`)
